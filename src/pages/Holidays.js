a5eb7b5b412335c428a6a8f190c4d209b0475019
Thomas-Rosenkrans-Vestergaard <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-11-13 09:46:28 +0100

    added basic html skeleton

---
+++ src/pages/Holidays.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Holidays extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div id="home-page">
+                <h2>Holidays</h2>
+            </div>
+        );
+    }
+}
+
+export default Holidays;
