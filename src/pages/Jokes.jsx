3ea34987f4908e38dfbd36ddc6f81b8649746deb
Jonas <jonasgroenbek@gmail.com>
2018-11-14 23:24:32 +0100

    fået funktionalitet i de sidste api'er undtagen hearthstone god damnit

--- src/pages/Jokes.jsx
+++ src/pages/Jokes.jsx
@@ -3,13 +3,36 @@ import React from "react";
 class Jokes extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { input: "", cards: [] };
+    this.state = { setup: "", punchline: ""};
   }
   render() {
-    return <div></div> ;
+    console.log(this.state);
+    return (
+      <div>
+        <button className="btn" onClick={this.clickHandler}>get a random joke!</button>
+        <table>
+          <thead>
+            <tr>
+              <th>setup</th>
+              <th>punchline</th>
+            </tr>
+          </thead>
+          <tbody>
+            <td>{this.state.setup}</td>
+            <td>{this.state.punchline}</td>
+          </tbody>
+        </table>
+      </div>
+    );
   }
 
-  getJokes() {}
+  clickHandler = () => {
+    fetch("http://localhost:8080/ca3/api/joke").then(response => {
+      response.json().then(joke => {
+        this.setState({setup: joke.setup, punchline: joke.punchline})
+      })
+    })
+  }
 }
 
 export default Jokes;
