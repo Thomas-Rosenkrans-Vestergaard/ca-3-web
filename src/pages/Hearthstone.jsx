f5a5cb1acff4c51147c41fa2ec2c9c9c1e709eab
Jonas <jonasgroenbek@gmail.com>
2018-11-14 12:02:43 +0100

    app.js lavet routes

---
+++ src/pages/Hearthstone.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Hearthstone extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {input: "", cards: []};
+  }
+  render() {
+    return;
+  }
+
+  getHearthstoneCards()
+}
+
+export default Hearthstone;
