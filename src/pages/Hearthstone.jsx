0318fdfd5562455db849ec8f9f2e39620acbd6d5
Jonas <jonasgroenbek@gmail.com>
2018-11-14 12:30:34 +0100

    hearthstone api

--- src/pages/Hearthstone.jsx
+++ src/pages/Hearthstone.jsx
@@ -3,14 +3,48 @@ import React from "react";
 class Hearthstone extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {input: "", cards: []};
+    this.state = { input: "", cards: [] };
   }
   render() {
-    return <div></div> ;
+    return (
+      <div>
+        <form onSubmit={onSubmit()}>
+          <input type="text" value={this.state.input} />
+          <input type=""></input>
+          <input type="submit" />
+        </form>
+        <table>
+          <thead />
+          <tbody />
+        </table>
+      </div>
+    );
   }
 
-  getHearthstoneCards(){
+  handleChange(event) {
+    this.setState({ input: event.target.value });
+  }
 
+  getHearthstoneCard(name) {
+    return fetch("heartstone/search/" + name).then(response => {
+      return response.json();
+    });
+  }
+  getHearthstoneCards() {
+    return fetch("heartstone/all").then(response => {
+      return response.json();
+    });
+  }
+  getRows() {
+    return this.state.pictures.map(picture => {
+      return ( 
+        <tr key={picture}>
+          <td>
+            <img src={picture} />
+          </td>
+        </tr>
+      );
+    });
   }
 }
 
